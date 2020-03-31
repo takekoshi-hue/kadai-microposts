fc71cff639d2340f0f5e11fcdd90c9ab29129734
masato.takekoshi <koshitake1@gmail.com>
2020-03-31 12:39:46 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,19 +13,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      flash[:success] = "ユーザを登録しました。"
+      flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
-      
     else
-      flash.now[:danger] = "ユーザの登録に失敗しました。"
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
+    end
   end
-end
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
