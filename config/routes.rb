0b6e84ee92a80d285db968365032de23852927a8
masato.takekoshi <koshitake1@gmail.com>
2020-04-01 12:37:11 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
   
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#creatr"
+  delete "logout", to: "sessions#destroy"
+  
+  
   get "signup", to: "users#new"
   resources :users, only: [:index, :show, :new, :create]
   end
