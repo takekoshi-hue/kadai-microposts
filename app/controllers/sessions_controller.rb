eab3dead48906e791dd0558fb9af6fed7c268308
masato.takekoshi <koshitake1@gmail.com>
2020-04-17 12:00:33 +0000

    follow, unfollo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+
 class SessionsController < ApplicationController
   def new
   end
@@ -6,29 +7,30 @@ class SessionsController < ApplicationController
     email = params[:session][:email].downcase
     password = params[:session][:password]
     if login(email, password)
-      flash[:success] = "ログインに成功しました。"
+      flash[:success] = 'ログインに成功しました。'
       redirect_to @user
     else
-      flash.now[:danger] = "ログインに失敗しました。"
+      flash.now[:danger] = 'ログインに失敗しました。'
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "ログアウトしました"
+    flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
-  
+
   private
-  
+
   def login(email, password)
     @user = User.find_by(email: email)
     if @user && @user.authenticate(password)
-      
+      # ログイン成功
       session[:user_id] = @user.id
       return true
     else
+      # ログイン失敗
       return false
     end
   end
